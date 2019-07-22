081695db3b4a16af7d3344fa5b3bc5622bc639c6
smitty02001 <jmsmith2001@gmail.com>
2019-07-22 11:29:44 -0400

    Setup to reproduce the sticky column error

--- app/table-sticky-columns-example.ts
+++ app/table-sticky-columns-example.ts
@@ -10,7 +10,7 @@ import {Component} from '@angular/core';
 })
 export class TableStickyColumnsExample {
   displayedColumns =
-      ['name', 'position', 'weight', 'symbol', 'position', 'weight', 'symbol', 'star'];
+      ['name', 'position', 'weight', 'symbol', 'position', 'weight', 'symbol', 'position', 'weight', 'symbol', 'position', 'weight', 'symbol', 'weight', 'symbol', 'position', 'weight', 'symbol', 'position', 'weight', 'symbol', 'weight', 'symbol', 'position', 'weight', 'symbol', 'position', 'weight', 'symbol', 'weight', 'symbol', 'position', 'weight', 'symbol', 'position', 'weight', 'symbol', 'weight', 'symbol', 'position', 'weight', 'symbol', 'position', 'weight', 'symbol', 'weight', 'symbol', 'position', 'weight', 'symbol', 'position', 'weight', 'symbol', 'yolo'];
   dataSource = ELEMENT_DATA;
 }
 
@@ -19,19 +19,20 @@ export interface PeriodicElement {
   position: number;
   weight: number;
   symbol: string;
+  yolo: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H', yolo: 'abc'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He', yolo: 'abc'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li', yolo: 'abc'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be', yolo: 'abc'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B', yolo: 'abc'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C', yolo: 'abc'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N', yolo: 'abc'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O', yolo: 'abc'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F', yolo: 'abc'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne', yolo: 'abc'},
 ];
 
 
